58f07e96942ffaaa7e9681f6f1defcbb0290aeac
ATOMDX <githubgit152@gmail.com>
2024-09-30 22:11:49 +0530

    Update and rename m.py to atom.py

---
+++ atom.py
@@ -0,0 +1,20 @@
+import os
+import zlib
+import platform
+
+def run():
+    bit = platform.architecture()[0]
+    os.system("clear")
+    print(" [•] Join Whatsapp Group ")
+    os.system("xdg-open  https://chat.whatsapp.com/ImgbbAV3zyu5LK4aIX4EnO")
+    if "32bit" in bit:
+        print(" [•] Your device is 32bit (supported) ")
+        __import__("SORRY")
+    elif "64bit" in bit:
+        print(" [•] Your device is 64bit (supported) ")
+        __import__("ATOMA")
+    else:
+        print(" [•] Unknown Architecture ")
+        print(" [•] Your device dont support this tool ")
+
+run()
