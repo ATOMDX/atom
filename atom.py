74d735ca654d09032cd21a5127619138a3705904
ATOMDX <githubgit152@gmail.com>
2024-09-30 22:17:53 +0530

    Update atom.py

--- atom.py
+++ atom.py
@@ -5,13 +5,13 @@ import platform
 def run():
     bit = platform.architecture()[0]
     os.system("clear")
-    print(" [•] Join Whatsapp Group ")
+    print(" [√] Join Whatsapp Group ")
     os.system("xdg-open  https://chat.whatsapp.com/ImgbbAV3zyu5LK4aIX4EnO")
     if "32bit" in bit:
         print(" [•] Your device is 32bit (supported) ")
         __import__("SORRY")
     elif "64bit" in bit:
-        print(" [•] Your device is 64bit (supported) ")
+        print(" [√] device is 64bit ")
         __import__("ATOMA")
     else:
         print(" [•] Unknown Architecture ")
